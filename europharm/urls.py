961050e952eb8cf73e60cd8a39b552511cd3e005
Daniyar Nurlanov <daniknurlanov0509@gmail.com>
2023-05-14 23:17:18 +0600

    Danik

--- europharm/urls.py
+++ europharm/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('edit_product/<int:id>/', edit_product.as_view(), name='edit_product'),
     path('edit_user/<int:id>/', edit_user.as_view(), name='edit_user'),
     path('search/', Search.as_view(), name='search'),
+    //asd
 ]
 
